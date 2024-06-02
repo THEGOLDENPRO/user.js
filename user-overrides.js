e05f2c1beb7cf8cdd27a534ea30d5e84dc6267eb
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-06-02 12:23:43 +0100

    feat: add user overrides js

---
+++ user-overrides.js
@@ -0,0 +1,5 @@
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("browser.display.use_system_colors", true);
+
+user_pref("browser.startup.homepage", "https://www.google.com");
